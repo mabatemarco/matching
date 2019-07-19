7b568bf75c3f35d9e0566504dfee3e470532bb3a
mabatemarco <mabatemarco928@gmail.com>
2019-07-19 14:34:52 -0400

    rnadom assignment

--- js/index.js
+++ js/index.js
@@ -1,7 +1,24 @@
+$(document).ready(game)
+function game(){
+	$('h2').remove();
+	$('button').remove();
+	$('img').show();
 var selecteda;
 var selectedb;
 var n=6;
-var cardturned=false
+var cardturned=false;
+var pics=['images/koda1.jpg','images/koda1.jpg','images/koda2.jpg','images/koda2.jpg','images/koda3.jpg','images/koda3.jpg','images/koda4.jpg','images/koda4.jpg','images/koda5.jpg','images/koda5.jpg','images/koda6.jpg','images/koda6.jpg',];
+var used=[];
+
+for(i=0;used.length<12;){
+	var x=Math.ceil(Math.random() * 12);
+	if(jQuery.inArray(x,used)==-1){
+		$('#'+x).attr('src',pics[i]);
+		used[i]=x;
+		++i;
+	}
+	else{}
+}
 
 var src1=$('#1').attr('src');
 var src2=$('#2').attr('src');
@@ -9,20 +26,29 @@ var src3=$('#3').attr('src');
 var src4=$('#4').attr('src');
 var src5=$('#5').attr('src');
 var src6=$('#6').attr('src');
+var src7=$('#7').attr('src');
+var src8=$('#8').attr('src');
+var src9=$('#9').attr('src');
+var src10=$('#10').attr('src');
+var src11=$('#11').attr('src');
+var src12=$('#12').attr('src');
 
 $('img').attr('src','images/card.png')
 
 $('img').click(match);
 
 function match(){
-	$('img').css('pointer-events', 'none');
+if($(this).attr('class')=='clicked'){
+	}
+else{
 	if(cardturned==true){
 		var srcb=eval('src'+$(this).attr('id'));
 		$(this).attr('src',srcb);
 		selectedb=$(this);
-		cardturned=false
+		cardturned=false;
+		$(selecteda).removeClass('clicked');
 		if($(this).attr('src')==$(selecteda).attr('src')){
-			setTimeout(pairget,1200)
+			setTimeout(pairget,800)
 		}
 		else{
 			setTimeout(reset,1200)
@@ -31,23 +57,24 @@ function match(){
 	else{
 		var srca=eval('src'+$(this).attr('id'));
 		$(this).attr('src',srca);
-		selecteda=(this);
+		selecteda=$(this);
 		cardturned=true;
-		$('img').css('pointer-events', 'auto');
+		$(this).addClass('clicked')
 	}
+}
 }
 
 function pairget(){
 	n-=1;
 	$(selecteda).hide();
 	$(selectedb).hide();
-	$('img').css('pointer-events', 'auto');
 	if(n==0){
-		$('#win').html("You won!");
+		$('#win').append('<h2>You won!</h2><br/><button type="button">New Game</button>');
 	}
 }
 
 function reset(){
 	$('img').attr('src','images/card.png');
-	$('img').css('pointer-events', 'auto');
 }
+}
+$(document).on('click','button',game)
