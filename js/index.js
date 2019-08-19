1f849d5e0e8355510e4fe67c32419bd864213467
mabatemarco <mabatemarco928@gmail.com>
2019-08-19 17:10:33 -0400

    nearly

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-$(document).ready(game)
-function game(){
-	$('h2').remove();
-	$('button').remove();
-	$('img').show();
 var selecteda;
 var selectedb;
 var n=6;
@@ -10,10 +5,38 @@ var cardturned=false;
 var pics=['images/koda1.jpg','images/koda1.jpg','images/koda2.jpg','images/koda2.jpg','images/koda3.jpg','images/koda3.jpg','images/koda4.jpg','images/koda4.jpg','images/koda5.jpg','images/koda5.jpg','images/koda6.jpg','images/koda6.jpg',];
 var used=[];
 var positions=[];
-var turns=0;
+var cscore=0;
 var delay=0;
 var srca;
 var srcb;
+var bscore=0;
+var src1;
+var src2;
+var src3;
+var src4;
+var src5;
+var src6;
+var src7;
+var src8;
+var src9;
+var src10;
+var src11;
+var src12;
+
+$(document).ready(game)
+function game(){
+$('h2').remove();
+$('button').remove();
+$('img').show();
+cscore=0;
+delay=0;
+$('#cscore').html(cscore);
+n=6;
+used=[];
+positions=[];
+srca=undefined;
+srcb=undefined;
+// Saves all sources
 for(i=0;used.length<12;){
 	var x=Math.ceil(Math.random() * 12);
 	if(jQuery.inArray(x,used)==-1){
@@ -22,30 +45,30 @@ for(i=0;used.length<12;){
 		++i;
 	}
 	else{}
-}
-var src1=$('#1').attr('src');
-var src2=$('#2').attr('src');
-var src3=$('#3').attr('src');
-var src4=$('#4').attr('src');
-var src5=$('#5').attr('src');
-var src6=$('#6').attr('src');
-var src7=$('#7').attr('src');
-var src8=$('#8').attr('src');
-var src9=$('#9').attr('src');
-var src10=$('#10').attr('src');
-var src11=$('#11').attr('src');
-var src12=$('#12').attr('src');
+};
+src1=$('#1').attr('src');
+src2=$('#2').attr('src');
+src3=$('#3').attr('src');
+src4=$('#4').attr('src');
+src5=$('#5').attr('src');
+src6=$('#6').attr('src');
+src7=$('#7').attr('src');
+src8=$('#8').attr('src');
+src9=$('#9').attr('src');
+src10=$('#10').attr('src');
+src11=$('#11').attr('src');
+src12=$('#12').attr('src');
 // Save positions
 $('img').each(function(index){
 	positions[index]=$(this).position()
-})
+});
 // Send all cards to center
 $('img').each(function(index){
 	$(this).animate({
 		left: 390,
 		top:230
 	},600)
-})
+});
 // Shuffle
 for(i=0;i<5;++i){
 	$('img').delay(50).each(function(index){
@@ -65,7 +88,7 @@ for(i=0;i<5;++i){
 		}
 		$('#shuffle')[0].play()
 	})
-}
+};
 // Send cards back to their places
 $('img').each(function(index){
 	$(this).delay(delay).animate({
@@ -73,25 +96,26 @@ $('img').each(function(index){
 		top: positions[index].top
 	},600)
 	delay+=100
-})
+});
 // Cards show back side
-$('img').attr('src','images/card.png')
+$('img').attr('src','images/card.png');
 // Main game
-$('img').click(match);
+$('img').on('click',match);
 function match(){
 if($(this).hasClass('clicked')){
 	}
 else{
-	$('#flip')[0].play()
+	$('#flip')[0].play();
 	if(cardturned==true){
-		turns+=1
+		cscore+=1
+		$('#cscore').html(cscore)
 		$('img').off('click')
 		srcb=eval('src'+$(this).attr('id'));
 		$(this).attr('src',srcb);
 		selectedb=$(this);
 		cardturned=false;
 		console.log(cardturned)
-		$(selecteda).removeClass('clicked');
+		$('img').removeClass('clicked');
 		if($(this).attr('src')==$(selecteda).attr('src')){
 			setTimeout(pairget,800)
 		}
@@ -117,6 +141,13 @@ function pairget(){
 	$(selectedb).hide();
 	if(n==0){
 		$('.container').prepend('<h2>You won!</h2><br/><button type="button">New Game</button>');
+		if (bscore==0){
+			bscore=cscore
+		}
+		else{
+			bscore=Math.min(bscore,cscore)
+		}
+		$('#bscore').html(bscore)
 	}
 }
 // When a pair is not found
